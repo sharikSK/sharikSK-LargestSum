4eae987e9cc9e4af563004aa97e759b2dc170e79
sharikSK <sharik10786@gmail.com>
2024-06-05 14:05:06 +0530

    final

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,6 @@
-
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LargestSum {
     /**
@@ -10,6 +11,20 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        Set<Integer> seen = new HashSet<>();
+        int maxSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i).equals(nums.get(j)) && !seen.contains(nums.get(i))) {
+                    int sum = nums.get(i) * 2; // Add the number to itself if it's a duplicate
+                    maxSum = Math.max(maxSum, sum);
+                    seen.add(nums.get(i));
+                } else if (!nums.get(i).equals(nums.get(j))) {
+                    int sum = nums.get(i) + nums.get(j);
+                    maxSum = Math.max(maxSum, sum);
+                }
+            }
+        }
+        return maxSum;
     }
 }
